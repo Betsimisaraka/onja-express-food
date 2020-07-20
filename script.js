abf271ccd228fc00a085faf58cb5a0298466acda
Anita RASOA <anita.ras@onja.org>
2020-07-20 09:39:37 +0300

    Trying to submit the button to get the new order

--- script.js
+++ script.js
@@ -6,65 +6,106 @@ const outerModal = document.querySelector('.outer-modal');
 const submitButton = document.querySelector('.submitOrder');
 const formReset = document.getElementsByTagName('form');
 const detailBtn = document.querySelector('.details');
-const inputName = document.getElementById('name');
 const inputAmount = document.querySelector('#quantity');
 const select = document.querySelector('.select-form');
 
 const handleButton = () => {
+    innerModal.innerHTML = `
+    <form>
+					<p>Your name :</p>
+					<input
+						class="input-form"
+						type="text"
+						id="name"
+						name="name"
+						placeholder="Enter your name here"
+						required
+					/>
+					<p>Please select your dish :</p>
+					<select name="dish" class="select-form" required>
+						<option value="romazava">Romazava</option>
+						<option value="koba">Koba</option>
+						<option value="ravitoto">Ravitoto</option>
+						<option value="mokary">Mokary</option>
+						<option value="achard">Achard</option>
+						<option value="masikita">Masikita</option>
+						<option value="sambos">Sambos</option>
+						<option value="mofo-baolina">Mofo baolina</option>
+						<option value="ranonapango">Ranonapango</option>
+					</select>
+					<p>Please select the size of your plate :</p>
+					<input type="radio" id="small" name="size" value="small" required />
+					<label for="small">Small</label><br />
+					<input type="radio" id="medium" name="size" value="medium" />
+					<label for="medium">Medium</label><br />
+					<input type="radio" id="large" name="size" value="large" />
+					<label for="large">Large</label><br />
+					<p>How many pieces do you want to order?</p>
+					<input
+						class="input-form"
+						type="number"
+						id="quantity"
+						name="amount"
+						placeholder="Enter a number here"
+						required
+					/>
+					<button class="submitOrder" type="submit">Add this order</button>
+				</form> 
+    `;
     outerModal.classList.add('open');
-}
+};
 
 addButton.addEventListener('click', handleButton);
 
-submitButton.addEventListener('click', (e) => {
-    
-    e.preventDefault();
-
-    const myHTML = `
+window.addEventListener('click', (event) => {
+    //To make the new order appear in the list of the order.
+    if (event.target.matches('button.submitOrder')) {
+        const inputName = document.getElementById('name');
+        const myHTML = `
         <div class="order" data-dish="koba" data-size="large" data-amount="1">
             <span class="title">${inputName.value}</span>
             <button class="details">Details</button>
             <button class="served">Delete order</button>
         </div>
     `;
-
+    event.target = myHTML;
     const orderList = document.querySelector('.order-list');
     orderList.insertAdjacentHTML('beforeend', myHTML);
     outerModal.classList.remove('open');
-});
-
-const deleteBtn = (event) => {
-    if (event.target.classList.contains('served')) {
+    }
+    else if (event.target.matches('.details')) {
+        innerModal.innerHTML = `
+          <h2>Anita</h2>
+          <p>Order: </p>
+          <p>2 small</p>
+        `;
+        outerModal.classList.add('open');
+    }
+    // To delete the order
+    else if (event.target.classList.contains('served')) {
         const deleteButton = event.target;
         deleteButton.closest('.order').remove();
     }
-}
+});
 
-document.addEventListener('click', deleteBtn);
+// document.addEventListener('click', deleteBtn);
 
 
 const detailButton = event => {
-    if (event.target.matches('.details')) {
-       innerModal.myDetails = `
-         <h2>${inputName.value}</h2>
-         <p>Order: </p>
-         <p>${inputAmount.value} ${inputSize.value} ${select.value}</p>
-       `;
-       outerModal.classList.add('open');
-    }
+    
 }
 
 
 
-let inputSize = document.getElementsByName('size');
-let sizeValue;
-for(var i = 0; i < inputSize.length; i++){
-    if(inputSize[i].checked){
-        sizeValue = inputSize[i].value;
-    }
-}
+// let inputSize = document.getElementsByName('size');
+// let sizeValue;
+// for(var i = 0; i < inputSize.length; i++){
+//     if(inputSize[i].checked){
+//         sizeValue = inputSize[i].value;
+//     }
+// }
 
-detailBtn.addEventListener('click', detailButton);
+// detailBtn.addEventListener('click', detailButton);
 
 
 
